6ce4d6cd3021e02da4f7963f2be72a56f457f9c2
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-12 22:17:50 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.6

---
+++ src/PyJuliaTester.jl
@@ -0,0 +1,5 @@
+module PyJuliaTester
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PyJuliaTester
+using Test
+
+@testset "PyJuliaTester.jl" begin
+    # Write your tests here.
+end
