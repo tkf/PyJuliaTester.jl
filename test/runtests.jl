8855853a0e64b1ff9c2aff94d8b715a3572300e5
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 17:44:03 -0700

    Compare test/Project.toml and test/environments/main/Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using PyJuliaTester
 using Test
 
+@testset "Compare test/Project.toml and test/environments/main/Project.toml" begin
+    @test Text(read(joinpath(@__DIR__, "Project.toml"), String)) ==
+          Text(read(joinpath(@__DIR__, "environments", "main", "Project.toml"), String))
+end
+
 @testset "PyJuliaTester.runtests" begin
     PyJuliaTester.runtests()
 end
